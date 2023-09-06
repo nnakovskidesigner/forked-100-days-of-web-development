68e1a5bcc2a04b35e1c23f74217b84c150089ece
nnakovskidesigner <117409706+nnakovskidesigner@users.noreply.github.com>
2023-09-06 16:34:00 +0200

    first fork
    test

---
+++ section-12/old-app.js
@@ -0,0 +1,9 @@
+let anchorElement = document.getElementById('external-link');
+anchorElement.href = 'https://yahoo.com';
+
+
+anchoeElement = document.querySelector('p a');
+anchorElement.href = 'https://academind.com';
+
+
+//creating a new element
